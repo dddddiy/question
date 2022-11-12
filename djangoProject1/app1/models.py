9e1c6593adde243f8e7faa8f3a6c57c82e86efc7
dddddiy <diy359666@163.com>
2022-11-12 14:21:07 +0800

    commit message

--- djangoProject1/app1/models.py
+++
@@ -1,9 +0,0 @@
-
-from django.db import models
-
-# Create your models here.
-class user(models.Model):
-    name = models.CharField(max_length=32,verbose_name="用户名")
-    #1
-    pwd = models.CharField(max_length=64,default="",verbose_name="密码")
-
